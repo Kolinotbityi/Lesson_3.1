afd0394f4749636d2103072d71c5dd87b6865b1c
Алеша <98235139+Kolinotbityi@users.noreply.github.com>
2024-09-29 22:07:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ calls = 0
 
 def count_calls():
     global calls
-    calls = 1
+    calls += 1
 
 def string_info(string):
     str_ = str(string)
     res_ = (len(str_), str_.upper(), str_.lower())
+    count_calls()
     return res_
 
 def is_contains(string, list_to_search):
     string = str(string).lower()
     list_to_search = list(list_to_search)
+    count_calls()
     for i in range(len(list_to_search)):
         if str(list_to_search[i]).lower() == string:
             res_ = True
